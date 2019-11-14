fb19a2dc61033f4ed4d63baa6c4d8f6a93200429
Huang, Hua <huangh223@gatech.edu>
2019-11-13 20:02:51 -0500

    Remove rotate_shells; load ref results from file

--- src/H2ERI_shell_operations.c
+++ src/H2ERI_shell_operations.c
@@ -8,85 +8,6 @@
 
 #include "H2ERI_typedef.h"
 
-// Rotate shell coordinates for better hierarchical partitioning
-// Input parameters:
-//   h2eri->nshell : Number of shells 
-//   h2eri->shells : Array, size nshell, shells to be rotated
-// Output parameters:
-//   h2eri->shells : Shells with rotated coordinates
-void H2ERI_rotate_shells(H2ERI_t h2eri)
-{
-    const int nshell = h2eri->nshell;
-    shell_t *shells  = h2eri->shells;
-    size_t col_msize = sizeof(double) * nshell;
-    double center[3] = {0.0, 0.0, 0.0};
-    double eigval[3], eigvec[9];
-    double *coord  = (double*) malloc(col_msize * 3);
-    double *coord1 = (double*) malloc(col_msize * 3);
-    assert(coord != NULL && coord1 != NULL);
-    
-    // 1. Rotate coordinates so the center is at the origin point & 
-    // the minimal bounding box of center points is parallel to unit box
-    for (int i = 0; i < nshell; i++)
-    {
-        coord[0 * nshell + i] = shells[i].x;
-        coord[1 * nshell + i] = shells[i].y;
-        coord[2 * nshell + i] = shells[i].z;
-        center[0] += shells[i].x;
-        center[1] += shells[i].y;
-        center[2] += shells[i].z;
-    }
-    double d_nshell = (double) nshell;
-    center[0] = center[0] / d_nshell;
-    center[1] = center[1] / d_nshell;
-    center[2] = center[2] / d_nshell;
-    for (int i = 0; i < nshell; i++)
-    {
-        coord[0 * nshell + i] -= center[0];
-        coord[1 * nshell + i] -= center[1];
-        coord[2 * nshell + i] -= center[2];
-    }
-    cblas_dgemm(
-        CblasColMajor, CblasTrans, CblasNoTrans, 3, 3, nshell,
-        1.0, coord, nshell, coord, nshell, 0.0, eigvec, 3
-    );
-    LAPACKE_dsyev(LAPACK_ROW_MAJOR, 'V', 'U', 3, eigvec, 3, eigval);
-    cblas_dgemm(
-        CblasColMajor, CblasNoTrans, CblasTrans, nshell, 3, 3, 
-        1.0, coord, nshell, eigvec, 3, 0.0, coord1, nshell
-    );
-    
-    // 2. Move the zero columns to the end
-    int col_idx = 0;
-    for (int i = 0; i < 3; i++)
-    {
-        double *col_ptr = coord1 + i * nshell;
-        double max_col_val = fabs(col_ptr[0]);
-        for (int j = 1; j < nshell; j++)
-        {
-            double val_j = fabs(col_ptr[j]);
-            if (val_j > max_col_val) max_col_val = val_j;
-        }
-        if (max_col_val > 1e-15)
-        {
-            double *dst_col = coord1 + col_idx * nshell;
-            if (col_idx != i) memcpy(dst_col, col_ptr, col_msize);
-            col_idx++;
-        }
-    }
-    
-    // 3. Update the center coordinates of shells
-    for (int i = 0; i < nshell; i++)
-    {
-        shells[i].x = coord1[0 * nshell + i];
-        shells[i].y = coord1[1 * nshell + i];
-        shells[i].z = coord1[2 * nshell + i];
-    }
-    
-    free(coord1);
-    free(coord);
-}
-
 // Fully uncontract all shells into new shells that each shell has
 // only 1 primitive function and screen uncontracted shell pairs
 // Input parameters:
@@ -304,7 +225,6 @@ void H2ERI_calc_unc_sp_extents(H2ERI_t h2eri)
 // Process input shells for H2 partitioning
 void H2ERI_process_shells(H2ERI_t h2eri)
 {
-    H2ERI_rotate_shells(h2eri);
     H2ERI_uncontract_shell_pairs(h2eri);
     H2ERI_calc_unc_sp_extents(h2eri);
 }
--- tests/test_H2ERI.c
+++ tests/test_H2ERI.c
@@ -24,52 +24,42 @@ int main(int argc, char **argv)
     // 4. Build H2 representation for ERI tensor
     H2ERI_build_H2(h2eri);
 
-    // 5. Generate a symmetric density matrix and save it to file
-    int num_bf = h2eri->num_bf;
-    double *den_mat = (double *) malloc(sizeof(double) * num_bf * num_bf);
-    double *J_mat   = (double *) malloc(sizeof(double) * num_bf * num_bf);
-    for (int i = 0; i < num_bf; i++)
-    {
-        double *dem_mat_row = den_mat + i * num_bf;
-        for (int j = 0; j < num_bf; j++)
-            dem_mat_row[j] = (double) ((i + j) % 11);
-    }
-    
-    FILE *ouf = fopen("add_c_x.m", "w");
-    fprintf(ouf, "c_x = [\n");
-    for (int i = 0; i < num_bf; i++)
-    {
-        double *dem_mat_row = den_mat + i * num_bf;
-        for (int j = 0; j < num_bf; j++)
-            fprintf(ouf, "%.16lf ", dem_mat_row[j]);
-        fprintf(ouf, "\n");
-    }
-    fprintf(ouf, "];");
-    fclose(ouf);
+    // 5. Read the reference density and Coulomb matrix from binary file
+    size_t nbf2 = h2eri->num_bf * h2eri->num_bf;
+    double *D_mat = (double *) malloc(sizeof(double) * nbf2);
+    double *J_mat = (double *) malloc(sizeof(double) * nbf2);
+    double *J_ref = (double *) malloc(sizeof(double) * nbf2);
+    FILE *D_file = fopen(argv[2], "r");
+    FILE *J_file = fopen(argv[3], "r");
+    fread(D_mat, nbf2, sizeof(double), D_file);
+    fread(J_ref, nbf2, sizeof(double), J_file);
+    fclose(D_file);
+    fclose(J_file);
     
     // 6. Construct the Coulomb matrix and save it to file
-    H2ERI_build_Coulomb(h2eri, den_mat, J_mat);  // Warm up
+    H2ERI_build_Coulomb(h2eri, D_mat, J_mat);  // Warm up
     h2eri->h2pack->n_matvec = 0;
     memset(h2eri->h2pack->timers + 4, 0, sizeof(double) * 5);
     for (int k = 0; k < 10; k++)
-         H2ERI_build_Coulomb(h2eri, den_mat, J_mat);
+         H2ERI_build_Coulomb(h2eri, D_mat, J_mat);
     
-    ouf = fopen("add_c_y.m", "w");
-    fprintf(ouf, "c_y = [\n");
-    for (int i = 0; i < num_bf; i++)
+    // 7. Calculate the relative error
+    double ref_l2 = 0.0, err_l2 = 0.0;
+    for (int i = 0; i < nbf2; i++)
     {
-        double *J_mat_row = J_mat + i * num_bf;
-        for (int j = 0; j < num_bf; j++)
-            fprintf(ouf, "%.16lf ", J_mat_row[j]);
-        fprintf(ouf, "\n");
+        double diff = J_ref[i] - J_mat[i];
+        ref_l2 += J_ref[i] * J_ref[i];
+        err_l2 += diff * diff;
     }
-    fprintf(ouf, "];");
-    fclose(ouf);
+    ref_l2 = sqrt(ref_l2);
+    err_l2 = sqrt(err_l2);
+    printf("||J_{H2} - J_{ref}||_2 / ||J_{ref}||_2 = %e\n", err_l2 / ref_l2);
     
     H2ERI_print_statistic(h2eri);
     
+    free(J_ref);
     free(J_mat);
-    free(den_mat);
+    free(D_mat);
     
     simint_finalize();
     return 0;
